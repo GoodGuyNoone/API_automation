070a4ed196cfab224d94f12a56da8d8bf7cc1748
Alexandr <Chur10@mail.ru>
2023-05-24 20:58:23 +0200

    minor changes

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -11,17 +11,22 @@ class TestCreatePlace():
         place_id = result_post.json().get("place_id")
         Checking.check_status_code(result_post, 200)
 
-        # print("Get Method -> Post")
-        # result_get = GoogleMapsApi.get_new_place(place_id)
-        #
-        # print("Put method")
-        # result_put = GoogleMapsApi.put_new_place(place_id)
-        #
-        # print("Get Method -> Put")
-        # result_get = GoogleMapsApi.get_new_place(place_id)
-        #
-        # print("Delete method")
-        # result_get = GoogleMapsApi.delete_new_place(place_id)
-        #
-        # print("Get Method -> Delete")
-        # result_get = GoogleMapsApi.get_new_place(place_id)
+        print("Get Method -> Post")
+        result_get = GoogleMapsApi.get_new_place(place_id)
+        Checking.check_status_code(result_get, 200)
+
+        print("Put method")
+        result_put = GoogleMapsApi.put_new_place(place_id)
+        Checking.check_status_code(result_put, 200)
+
+        print("Get Method -> Put")
+        result_get = GoogleMapsApi.get_new_place(place_id)
+        Checking.check_status_code(result_get, 200)
+
+        print("Delete method")
+        result_delete = GoogleMapsApi.delete_new_place(place_id)
+        Checking.check_status_code(result_delete, 200)
+
+        print("Get Method -> Delete")
+        result_get = GoogleMapsApi.get_new_place(place_id)
+        Checking.check_status_code(result_get, 404)
